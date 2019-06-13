0f3686c42de8e22ddcf7935e55387954d2e1edf4
FernandaSzAdr <fsandrade25@gmail.com>
2019-06-12 22:44:51 -0300

    Atualiza a extrutura do código principal
    Remove função não utilizadas
    Adiciona novas funções

--- expression_recog/recognition.py
+++ expression_recog/recognition.py
@@ -1,69 +1,87 @@
 # -*- coding: utf-8 -*-
 
+from helpers.mlp import MLP
+from helpers.patches_face import PatchesFace
+from helpers.read_jafee import read_samples_jafee
+
 import cv2
-from helpers.read_jafee import read_samples
-from helpers.local_binary_pattern import LocalBinaryPatterns
-from helpers.zernike_moments import ZernikeMoments
-from sklearn.metrics import accuracy_score
-from sklearn.svm import LinearSVC
+import dlib
+from imutils import face_utils
 
 
 def convertToRGB(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
-def get_face(file):
+def get_face(file, detector, predictor):
     image = cv2.imread(file, 0)
     image = convertToRGB(image)
+    image = cv2.GaussianBlur(image, (3, 3), 0)
 
-    haar_face = cv2.CascadeClassifier(
-            'DataSets/haarcascades_files/haarcascade_frontalface_default.xml')
-    coordenates_face = haar_face.detectMultiScale(image, scaleFactor=1.2,
-                                                  minNeighbors=5)
+    rects = detector(image, 1)
 
-    if len(coordenates_face) != 1:
-        raise ValueError('Número de faces detectadas foi incorreta para '
+    if len(rects) != 1:
+        raise ValueError('Número de faces detectadas foi incorreta para a '
                          'imagem {}'.format(file))
 
-    x, y, w, h = coordenates_face[0]
-    return image[y:y+h, x:x+w]
+    (x, y, w, h) = face_utils.rect_to_bb(rects[0])
+    face = image[y:y+h, x:x+w]
+
+    for (i, rect) in enumerate(rects):
+        shape = predictor(image, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        if len(shape) != 68:
+            raise ValueError('Número de pontos na face foi incorreta para a '
+                             'imagem {}'.format(file))
+
+    return face, PatchesFace(shape, image)
 
 
 def read_files(images):
-    descritor_lbp = LocalBinaryPatterns(12, 4)
-    descritor_zrnike = ZernikeMoments(21)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('helpers/'
+                                     'shape_predictor_68_face_landmarks.dat')
+
     datas = []
 
     for file in images:
-        face = get_face(file)
-        face = cv2.cvtColor(face, cv2.COLOR_RGB2GRAY)
-        histogram = descritor_lbp.describe(face)
+        data = []
+        face, patches_face = get_face(file, detector, predictor)
 
-        outline = descritor_zrnike.segment_image(face)
-        moments = descritor_zrnike.describe(outline)
-        # todo: testar o parametro inicial e um metodo de limiarização
-        datas.append(histogram)
+        data.extend(patches_face.patch_p1())
+        data.extend(patches_face.patch_p2())
+        data.extend(patches_face.patch_p4())
+        data.extend(patches_face.patch_p5())
+        data.extend(patches_face.patch_p16())
+        data.extend(patches_face.patch_p18())
+        data.extend(patches_face.patch_p19())
+
+        data.extend(patches_face.patch_mouth())
+        data.extend(patches_face.patch_eyebrow())
+
+        datas.append(data)
 
     return datas
 
 
-def test_images(model, test_image_x):
-    test_x = read_files(test_image_x)
-    result_obt = []
-
-    for i, image in enumerate(test_x):
-        result_obt.append(model.predict(image.reshape(1, -1))[0])
-
-    return result_obt
-
-
 if __name__ == '__main__':
-    test_image_x, test_y, train_image_x, train_y = read_samples()
+    print('################################################################')
+    print('Read samples .....')
+    test_image_x, test_y, train_image_x, train_y = read_samples_jafee()
 
+    print('################################################################')
+    print('Prepare samples .....')
     train_x = read_files(train_image_x)
-    model = LinearSVC()
-    model.fit(train_x, train_y)
+    test_x = read_files(test_image_x)
 
-    test_obt = test_images(model, test_image_x)
+    mlp = MLP(x_train=train_x, y_train=train_y, x_test=test_x, y_test=test_y)
+    mlp.compile_model()
+    print('################################################################')
+    print('Train model .....')
+    mlp.fit_model()
 
-    print('Acurracy {0:.2f}%'.format(accuracy_score(test_y, test_obt)*100))
+    print('################################################################')
+    score = mlp.evaluate_model()
+    print('Test loss: {}'.format(score[0]))
+    print('Test accuracy: {}%'.format(score[1] * 100))
