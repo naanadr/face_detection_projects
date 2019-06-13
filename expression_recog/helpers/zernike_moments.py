3e82d7cb8c0676283fac2369d7049fb890a289b0
FernandaSzAdr <fsandrade25@gmail.com>
2019-06-12 22:45:32 -0300

    Remove o redimensionamento realizado antes da binarização

--- expression_recog/helpers/zernike_moments.py
+++ expression_recog/helpers/zernike_moments.py
@@ -13,9 +13,8 @@ class ZernikeMoments:
         return mahotas.features.zernike_moments(image, self.radius)
 
     def segment_image(self, image):
-        image = cv2.copyMakeBorder(image, 10, 10, 10, 10,
-                                   cv2.BORDER_CONSTANT, value=0)
         thresh = cv2.adaptiveThreshold(image, 255,
                                        cv2.ADAPTIVE_THRESH_MEAN_C,
                                        cv2.THRESH_BINARY_INV, 5, 8)
+
         return thresh
